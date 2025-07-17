6c1cae47dfa10a3df824fa8b804de9fd5884a417
Derek Meulmeester <derek.meulmeester@gmail.com>
2025-07-16 23:00:47 -0400

    More styling

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -5,16 +5,16 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-5xl">{children}</h1>
+      <h1 className="text-5xl mb-2">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-4xl">{children}</h2>
+      <h2 className="text-4xl mb-2">{children}</h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-3xl">{children}</h3>
+      <h3 className="text-3xl mb-2">{children}</h3>
     ),
     h4: ({ children }) => (
-      <h4 className="text-2xl">{children}</h4>
+      <h4 className="text-2xl mb-2">{children}</h4>
     ),
     hr: () => <hr className="border-gray-200" />,
     a: ({ children, href }) => (
